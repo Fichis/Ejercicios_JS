cb8b8ba611a2a941048d375354abd3d49554d339
Adolfo <adolfoinsti@gmail.com>
2024-10-05 13:47:31 +0200

    Relacion BOM menos ultimo ejercicio

---
+++ Tema 2/Relacion5(BOM)/funciones.js
@@ -0,0 +1,13 @@
+function abreVentana(numVentanas, wid, hgh, url) {
+  let ancho = window.innerWidth;
+  for (let i = 0; i < numVentanas; i++) {
+    ventana = window.open(
+      url,
+      "Ventana " + i,
+      "width = " + wid + ", height = " + hgh + ", top = 0, left = " + ancho
+    );
+    ventana.document.write(
+      "<button onclick = 'window.close()'>Cerrar Ventana</button>"
+    );
+  }
+}
