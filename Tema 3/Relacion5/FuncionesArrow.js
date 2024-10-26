c4e81e9b4dfefd498c43bc06ec198c89be9860bd
Adolfo <adolfoinsti@gmail.com>
2024-10-27 00:16:49 +0200

    agregado Relacion function arrays

---
+++ Tema 3/Relacion5/FuncionesArrow.js
@@ -0,0 +1,4 @@
+const sum = (num1, num2) => num1 + num2;
+const stringLength = str => "El tamaño de la cadena es: " + str.length;
+const stringLength2 = str => "El tamaño de la cadena es: " + str.length;
+const showAlert = (name) => alert(alerts[Math.floor(Math.random() * alerts.length)] + `, ${name}!`);
